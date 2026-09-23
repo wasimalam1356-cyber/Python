7681255d30802416cb322e86febfdd931f50f18d
Wasim Alam <wasimalam1356@gmail.com>
2026-09-23 16:08:54 +0530

    Snake, Water, Gun game
    Implemented a simple game of Snake, Water, Gun where the user plays against the computer. The game randomly selects a choice for the computer and compares it with the user's input to determine the winner.

--- Project1.py
+++ Project1.py
@@ -1,1 +1,31 @@
 
+import random
+'''
+1 for snake
+-1 for water
+0 for  gun
+'''
+computer= random.choice([-1,0,1])
+youstr= input("Enter your choice: ")
+youdict= {"s": 1, "w":-1, "g":0}
+reversedict={1:"Snake", -1:"Water", 0:"Gun"}
+you=youdict[youstr]
+print(f"You chose {reversedict[you]} and Computer chose {reversedict[computer]}")
+if(computer==you):
+    print("It's a draw")
+else:
+    if(computer==-1 and you==1):
+       print("You win!")
+    elif(computer==-1 and you==0):
+       print("You Lose!")
+    elif(computer==1 and you==-1):
+       print("You Lose!")
+    elif(computer==1 and you==0):
+       print("You Win!")
+    elif(computer==0 and you==-1):
+       print("You Lose!")
+    elif(computer==0 and you==1):
+       print("You Win!")
+
+    else:
+       print("Something went wrong!")
